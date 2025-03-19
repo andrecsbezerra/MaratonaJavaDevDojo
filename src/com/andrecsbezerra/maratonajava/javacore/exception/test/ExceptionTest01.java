bb92b0154551e8cf6cd436332fe195e219568368
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-19 18:54:25 -0300

    feat: add new file ExceptionTest01

---
+++ src/com/andrecsbezerra/maratonajava/javacore/exception/test/ExceptionTest01.java
@@ -0,0 +1,32 @@
+package com.andrecsbezerra.maratonajava.javacore.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+        boolean criarArquivo = true;
+        boolean deletarArquibo = false;
+        if (criarArquivo) {
+            criarNovoArquivo();
+        }
+        if (deletarArquibo){
+            deletarNovoArquivo();
+        }
+    }
+
+    private  static void criarNovoArquivo(){
+        File file = new File("arquivo\\teste.txt");
+        try{
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado " + isCriado);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void deletarNovoArquivo(){
+        File file = new File("arquivo\\teste.txt");
+        file.delete();
+    }
+}
