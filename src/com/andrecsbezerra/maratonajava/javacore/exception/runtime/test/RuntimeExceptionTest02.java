e134a2c2c9df95cfb7d07a6ba6c529191a289ae8
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-21 07:16:59 -0300

    feat: add comments RuntimeExceptionTest02

--- src/com/andrecsbezerra/maratonajava/javacore/exception/runtime/test/RuntimeExceptionTest02.java
+++ src/com/andrecsbezerra/maratonajava/javacore/exception/runtime/test/RuntimeExceptionTest02.java
@@ -5,6 +5,13 @@ public class RuntimeExceptionTest02 {
         divisao(1,0);
     }
 
+    /**
+     *
+     * @param a
+     * @param b divisor não pode ser zero
+     * @return
+     * @throws IllegalArgumentException caso b seja igual a zeo
+     * **/
     private static int divisao(int a, int b){
         if (b == 0){
             throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
