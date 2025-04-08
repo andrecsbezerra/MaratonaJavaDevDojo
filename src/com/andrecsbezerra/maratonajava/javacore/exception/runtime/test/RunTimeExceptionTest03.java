bb37b8f4a8d7859d6fac8cd58349d88abc2f116e
André Bezerra <andrebezerracomercial@gmail.com>
2025-04-08 18:16:24 -0300

    feat: add finally block

---
+++ src/com/andrecsbezerra/maratonajava/javacore/exception/runtime/test/RunTimeExceptionTest03.java
@@ -0,0 +1,20 @@
+package com.andrecsbezerra.maratonajava.javacore.exception.runtime.test;
+
+public class RunTimeExceptionTest03 {
+    public static void main(String[] args) {
+        abreConexao();
+    }
+
+    private static String abreConexao() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no Arquivo");
+            return "conexao aberta";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+        return null;
+    }
+}
