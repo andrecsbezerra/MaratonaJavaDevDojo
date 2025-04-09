0f5f3c2cd95b6112cb2912aeb7c7234bc933bb4b
André Bezerra <andrebezerracomercial@gmail.com>
2025-04-09 18:59:37 -0300

    fix: add Strings pt.3

---
+++ src/com/andrecsbezerra/maratonajava/javacore/string/test/StringPerformanceTest.java
@@ -0,0 +1,42 @@
+package com.andrecsbezerra.maratonajava.javacore.string.test;
+
+public class StringPerformanceTest {
+    public static void main(String[] args) {
+        long incio = System.currentTimeMillis();
+        concatString(100_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para string " + (fim - incio) + "ms");
+
+
+        incio = System.currentTimeMillis();
+        concatStringBuilder(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para string " + (fim - incio) + "ms");
+
+        incio = System.currentTimeMillis();
+        concatStringBuffer(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para string " + (fim - incio) + "ms");
+    }
+
+    static void concatString(int tamanho){
+        String texto = "";
+        for (int i = 0; i < tamanho; i++) {
+            texto += i; // 0 \ 01 \ 012 \ 0123
+        }
+    }
+
+    static void concatStringBuilder(int tamanho){
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+
+    static void concatStringBuffer(int tamanho){
+        StringBuffer sb = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+}
