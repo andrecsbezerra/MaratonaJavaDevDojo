742a58708bcef153dc2f88283458996eff54a2f9
André Bezerra <andrebezerracomercial@gmail.com>
2025-04-15 17:14:50 -0300

    fix: add Comments

--- src/com/andrecsbezerra/maratonajava/javacore/io/test/FileReaderTest01.java
+++ src/com/andrecsbezerra/maratonajava/javacore/io/test/FileReaderTest01.java
@@ -8,11 +8,11 @@ public class FileReaderTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileReader fr = new FileReader(file)) {
-            char[] in = new char[10];
-            fr.read(in);
-            for(char c : in) {
-                System.out.print(c);
-            }
+//            char[] in = new char[10];
+//            fr.read(in);
+//            for(char c : in) {
+//                System.out.print(c);
+//            }
             int i;
             while((i=fr.read()) != -1) {
                 System.out.print((char)i);
